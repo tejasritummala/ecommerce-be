73afb27cbcc23845a19202bba53aa5ca369c3f57
tejasritummala <tejasritummala@gmail.com>
2024-07-13 17:09:14 -0500

    Added CORS configuration to allow requests from http://localhost:8080

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var express = require('express');
+var express = require('express')
+var cors = require('cors')
 const DBConnect = require('./config/db')
 const ProductsModel = require('./models/Products')
 
 var app = express()
+app.use(cors({ origin: 'http://localhost:8080' }))
 DBConnect()
 
 app.get('/api/products', async (req, res) => {
-    try {
-      const products = await ProductsModel.find({});
-      res.json(products);
-    } catch (err) {
-      res.status(500).send(err);
-    }
+  try {
+    const products = await ProductsModel.find({});
+    res.json(products);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.listen(8000, () =>{
-    console.log("Server is listening to port 8000")
+app.listen(8000, () => {
+  console.log("Server is listening to port 8000")
 })
